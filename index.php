c5ab33e3ec370334a297a7b52178360822a85dd8
Lukas Kleinschmidt <kleinschmidt@eatersvz.de>
2020-03-19 15:55:05 +0100

    Cleanup

--- index.php
+++ index.php
@@ -7,99 +7,93 @@ Kirby::plugin('lukaskleinschmidt/resolve', [
         'cache' => true,
     ],
     'hooks' => [
-        'system.loadPlugins:after' => function () {
-            kirby()->extend([
-                'hooks' => [
-                    'page.changeSlug:before' => function ($page) {
-                        kirby()->cache('lukaskleinschmidt.resolve')->flush();
-                    },
-                    'route:before' => function ($route, $path, $method) {
-                        if (option('lukaskleinschmidt.resolve.cache') === false) {
-                            return;
+        'page.changeSlug:before' => function ($page) {
+            kirby()->cache('lukaskleinschmidt.resolve')->flush();
+        },
+        'route:before' => function ($route, $path, $method) {
+            if (option('lukaskleinschmidt.resolve.cache') === false) {
+                return;
+            }
+
+            $kirby = kirby();
+            $proxy = $kirby->cache('lukaskleinschmidt.resolve')
+                            ->get($path, false);
+
+            if ($proxy === false) {
+                return;
+            }
+
+            $parts  = explode('/', $proxy['path']);
+            $root   = $kirby->root('content');
+            $parent = null;
+            $page   = null;
+
+            foreach ($parts as $part) {
+                $root .= '/' . $part;
+
+                if ($part === '_draft') {
+                    continue;
+                }
+
+                if (preg_match('/^([0-9]+)_(.*)$/', $part, $match)) {
+                    $num  = $match[1];
+                    $slug = $match[2];
+                } else {
+                    $num  = null;
+                    $slug = $part;
+                }
+
+                $params = [
+                    'root'   => $root,
+                    'parent' => $parent,
+                    'slug'   => $slug,
+                    'num'    => $num,
+                ];
+
+                if (empty(Page::$models) === false) {
+                    $extension = $kirby->contentExtension();
+
+                    if ($kirby->multilang()) {
+                        $extension = $kirby->defaultLanguage()->code() . '.' . $extension;
+                    }
+
+                    foreach (array_keys(Page::$models) as $model) {
+                        if (file_exists($params['root'] . '/' . $model . '.' . $extension) === true) {
+                            $params['model'] = $model;
+                            break;
                         }
+                    }
+                }
 
-                        $kirby = kirby();
-                        $proxy = $kirby->cache('lukaskleinschmidt.resolve')
-                                       ->get($path, false);
+                $parent = $page = Page::factory($params);
+            }
 
-                        if ($proxy === false) {
-                            return;
-                        }
-
-                        $parts  = explode('/', $proxy['path']);
-                        $root   = $kirby->root('content');
-                        $parent = null;
-                        $page   = null;
-
-                        foreach ($parts as $part) {
-                            $root .= '/' . $part;
-
-                            if ($part === '_draft') {
-                                continue;
-                            }
-
-                            if (preg_match('/^([0-9]+)_(.*)$/', $part, $match)) {
-                                $num  = $match[1];
-                                $slug = $match[2];
-                            } else {
-                                $num  = null;
-                                $slug = $part;
-                            }
-
-                            $params = [
-                                'root'   => $root,
-                                'parent' => $parent,
-                                'slug'   => $slug,
-                                'num'    => $num,
-                            ];
-
-                            if (empty(Page::$models) === false) {
-                                $extension = $kirby->contentExtension();
-
-                                if ($kirby->multilang()) {
-                                    $extension = $kirby->defaultLanguage()->code() . '.' . $extension;
-                                }
-
-                                foreach (array_keys(Page::$models) as $model) {
-                                    if (file_exists($params['root'] . '/' . $model . '.' . $extension) === true) {
-                                        $params['model'] = $model;
-                                        break;
-                                    }
-                                }
-                            }
-
-                            $parent = $page = Page::factory($params);
-                        }
-
-                        $kirby->extend([
-                            'pages' => [$path => $page]
-                        ]);
-                    },
-                    'route:after' => function ($route, $path, $method, $result) {
-                        if (option('lukaskleinschmidt.resolve.cache') === false) {
-                            return;
-                        }
-
-                        $kirby = kirby();
-                        $cache = $kirby->cache('lukaskleinschmidt.resolve');
-                        $proxy = $cache->get($path, false);
-
-                        if ($proxy !== false) {
-                            $kirby->setCurrentTranslation($proxy['lang']);
-                            $kirby->setCurrentLanguage($proxy['lang']);
-                        }
-
-                        if ($proxy === false && is_a($result, 'Kirby\Cms\Page')) {
-                            $cache->set($path, [
-                                'path' => $result->diruri(),
-                                'lang' => $kirby->languageCode(),
-                            ]);
-                        }
-
-                        return $result;
-                    },
-                ]
+            $kirby->extend([
+                'pages' => [$path => $page]
             ]);
-        }
+        },
+        'route:after' => function ($route, $path, $method, $result) {
+            if (option('lukaskleinschmidt.resolve.cache') === false) {
+                return $result;
+            }
+
+            $kirby = kirby();
+            $cache = $kirby->cache('lukaskleinschmidt.resolve');
+            $proxy = $cache->get($path, false);
+
+            if ($proxy !== false) {
+                $kirby->setCurrentTranslation($proxy['lang']);
+                $kirby->setCurrentLanguage($proxy['lang']);
+            }
+
+            if ($proxy === false && is_a($result, 'Kirby\Cms\Page')) {
+                $cache->set($path, [
+                    'path' => $result->diruri(),
+                    'lang' => $kirby->languageCode(),
+                ]);
+            }
+
+            return $result;
+        },
     ]
 ]);
